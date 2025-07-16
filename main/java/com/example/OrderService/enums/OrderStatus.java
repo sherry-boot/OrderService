766680877ce88bab2b3db256a191277a27b448f0
sherry-boot <shakeebshahid9@gmail.com>
2025-07-16 15:54:55 +0530

    Delete main directory

--- main/java/com/example/OrderService/enums/OrderStatus.java
+++
@@ -1,8 +0,0 @@
-package com.example.OrderService.enums;
-
-public enum OrderStatus {
-
-    PENDING,
-    CANCELLED,
-    COMPLETED
-}
--- main/java/com/example/OrderService/repository/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.OrderService.repository;
-
-import com.example.OrderService.entity.OrderItems;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItems, Long> {
-}
